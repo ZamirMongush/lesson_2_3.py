09611c36b695366b38ab403f6d7be70dcf2502c4
ZamirMongush <zmongush86@gmail.com>
2024-11-27 22:04:37 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,46 @@
+# import random
+# from random import choice
+#
+# def lottery(*args):
+#     tickets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     win1 = random.choice(tickets)
+#     tickets.remove(win1)
+#     win2 = random.choice(tickets)
+#     print(*args)
+#     return win1, win2
+#
+# win1, win2 = lottery('*args', 1,2,3,4,5,6,7,8,9,10)
+# print(win1, win2)
+
+# def test(a=2,b=True):
+#     print(a,b)
+#
+# test(False, 'ok')
+
+# def test(a=2, b=True):
+#     print(a, b)
+#
+#
+# test([1, 2])
+
+# def test(a=2, b=True):
+#     print(a, b)
+#
+#
+# test(*[1, 2])
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+result1 = get_matrix(2,2,10)
+result2 = get_matrix(3,5,42)
+result3 = get_matrix(4,2,13)
+print(result1)
+print(result2)
+print(result3)
+
